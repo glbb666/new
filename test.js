a5d63de4bed659915daec4690e3f1c739ef24496
glbb666 <menghuangjingy@126.com>
2019-03-27 22:50:33 +0800

    。、

--- test.js
+++
@@ -1,9 +0,0 @@
-//在下一次事件响应中执行代码
-process.nextTick(function(){
-    console.log('nextTick callback!')
-})
-console.log('nextTick was set')
-//程序即将退出时的回调函数
-process.on('exit',function(code){
-    console.log('about to exit with code:'+code);
-})
