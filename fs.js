a681884b386e8977ecf0fff8c9d00f95ca12630a
glbb666 <menghuangjingy@126.com>
2019-03-28 09:18:13 +0800

    BUFFER

--- fs.js
+++ fs.js
@@ -27,11 +27,21 @@ fs.readFile('sample.txt','utf-8',function(err,data){
 console.log('不等待IO结果直接执行后续代码...');
 
 //异步读取图片文件
-fs.readFile('1.jfif',function(err,data){
+fs.readFile('1.jpg',function(err,data){
     if(err){
         console.log(err);
     }else{
-        console.log('文件为:'+data);
-        console.log('文件的大小为:'+data.length+'bytes')
+        //当读取二进制文件时，不传入文件编码时，回调函数的data参数将返回一个Buffer对象。在Node.js中，Buffer对象就是一个包含零个字节或任意个字节的数组(注意和Array不同)
+        console.log(data);
+        console.log('文件的大小为:'+data.length+'bytes');
+        // Buffer对象可以和String做转换,例如,把一个Buffer对象转换成String
+        // Buffer->String
+        var text = data.toString('utf-8');
+console.log(text);
+        var buf = Buffer.from(text,'utf-8');
+        console.log(buf)
     }
 })
+
+
+
