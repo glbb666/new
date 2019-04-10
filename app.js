421a461ecd1baa771a50212017f397ce6896833b
glbb666 <menghuangjingy@126.com>
2019-04-10 20:31:57 +0800

    .

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-console.log(
-    "This is a test string."
-    );
-console.time('small loop');    
-// console.error("This is anerror string.");
-console.warn("this is a warn");
-for(i = 0;i<100000;i++){
-    ;
-}
-console.timeEnd('small loop');
-// console.trace('trace');
-console.assert(1==false,'error');
+// console.log(
+//     "This is a test string."
+//     );
+// console.time('small loop');    
+// // console.error("This is anerror string.");
+// console.warn("this is a warn");
+// for(i = 0;i<100000;i++){
+//     ;
+// }
+// console.timeEnd('small loop');
+// // console.trace('trace');
+// console.assert(1==false,'error');
+var testModule = require('./testModule.js');
+if(module === require.main) {
+    console.log('This is the main module of application.');
+  }
+require.resolve('./testModule.js')
--- testModule.js
+++ testModule.js
@@ -1,2 +1,7 @@
 var testVar="This is a variable from testModule.js.";
+console.log(testVar);
+
 exports.testVar =testVar;
+if(module === require.main) {
+    console.log('This is the main module of application.');
+  }
