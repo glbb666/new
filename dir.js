b10620426bed283bd213c62bd0a717b51e5e0317
glbb666 <menghuangjingy@126.com>
2019-04-11 22:35:49 +0800

    。。

--- dir.js
+++ dir.js
@@ -1,9 +1,32 @@
+// var fs = require('fs');
+// fs.mkdir('./newTest',function(err){
+//     if(err) console.log('创建目录操作失败');
+//     else console.log('创建目录操作成功')
+// })
+// fs.readdir('./test',function(err,files){
+//     if(err) console.log('读取目录失败')
+//     else console.log(files)
+// })
+// var files = fs.readdirSync('./test');
+// console.log(files)
+
+// 查看与修改文件或目录的信息
 var fs = require('fs');
-fs.mkdir('./newTest',function(err){
-    if(err) console.log('创建目录操作失败');
-    else console.log('创建目录操作成功')
-})
-fs.readdir('./test',function(err,files){
-    if(err) console.log('读取目录失败')
-    else console.log(files)
-})
+// fs.stat('sample.txt',function(err,stats){
+//     console.log(stats)
+// })
+// var state = fs.statSync('sample.txt');
+// console.log(state);
+// fs.open('sample.txt','r',function(err,fd){
+//     fs.fstat(fd,function(err,stats){
+//         console.log(stats)
+//     })
+// })
+
+//检查文件或者目录是否存在
+// fs.exists('sample.txt',function(exists){
+//     if(exists) console.log('该文件存在')
+//     else console.log('该文件不存在')
+// })
+
+//获取文件或者目录的绝对路径
