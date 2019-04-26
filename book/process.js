a40c8fb7944d8dd4e38c3e83bb9f7342cb4e9b5d
glbb666 <menghuangjingy@126.com>
2019-04-26 22:16:39 +0800

    .

--- book/process.js
+++ book/process.js
@@ -231,22 +231,42 @@
 // console.log('cluster.setting 属性值:%j',cluster.settings);
 
 //在多个子进程中运行HTTP服务器
+// var cluster = require('cluster');
+// var http = require('http')
+// if(cluster.isMaster){
+//     cluster.fork();
+//     cluster.fork();
+// }
+// else{
+//     http.createServer(function(req,res){
+//         if(req.url!=="/favicon.ico"){
+//             var sum = 0;
+//             for(var i = 0;i<5000000;i++){
+//                 sum+=i;
+//             }
+//             res.writeHead(200);
+//             res.write('客户端请求在子进程'+cluster.worker.id+"中被处理")
+//             res.end('sum='+sum)
+//         }
+//     }).listen(1337);
+// }
+
+//设定子进程对象使用独立的标准输入/输出
 var cluster = require('cluster');
-var http = require('http')
+var http = require('http');
 if(cluster.isMaster){
-    cluster.fork();
-    cluster.fork();
+    cluster.setupMaster({
+        silent:true
+    });
+    var worker = cluster.fork();
+    worker.process.stdout.on('data',function(data){
+        console.log('接受到来自客户端请求，目标地址为:'+data);
+    });
 }
 else{
     http.createServer(function(req,res){
-        if(req.url!=="/favicon.ico"){
-            var sum = 0;
-            for(var i = 0;i<5000000;i++){
-                sum+=i;
-            }
-            res.writeHead(200);
-            res.write('客户端请求在子进程'+cluster.worker.id+"中被处理")
-            res.end('sum='+sum)
+        if(req.url!=='favicon.ico'){
+            res.
         }
-    }).listen(1337)
+    })
 }
