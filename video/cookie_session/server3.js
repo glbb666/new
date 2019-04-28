e94dc887597596bda2bdf5988d147f090deaf2d6
glbb666 <menghuangjingy@126.com>
2019-04-28 15:55:40 +0800

    .

--- video/cookie_session/server3.js
+++ video/cookie_session/server3.js
@@ -4,13 +4,17 @@ var server = express();
 
 //cookie
 // 1.读取——cookie-parser
-// 2.发送——
-server.use(cookieParser())
+// 2.发送——cookie(名字,值,{path:'/',maxAge:毫秒,signed})
+server.use(cookieParser('dsgfdgfd'))
 
 server.use('/aaa',function(req,res){
     // console.log(req.cookies)
     req.secret = 'dsgfdgfd'
-    res.cookie('user','blue')
+    res.cookie('user','blue',{signed:true})
+
+    console.log('签名cookie:',req.signedCookies)
+    console.log('无签名cookie:',req.cookies)
+    
     res.send('ok')
 })
 
